2776675d38f3e4aeaeba1a9072b4f0ccf2c1243e
PemaLW <pemalw99@gmail.com>
2023-10-01 00:25:47 +0600

    adding the initialized docs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-xl'>Hello World</h1>
+    <div className='flex flex-row'>
+      Hello this is cool
+      <div className='h-[50px] w-[50px] bg-[#444444]'> uu </div>
+    </div>
+    </>
+  )
+}
+
+export default App
