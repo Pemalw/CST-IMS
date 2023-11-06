6d3aa94d9511ed3980c3adb6da78d392d4ab98f0
Sonamdema0810 <110452578+Sonamdema0810@users.noreply.github.com>
2023-11-07 01:54:32 +0400

    post change

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,46 +1,47 @@
 import { Link } from "react-router-dom";
-// import "./post.css";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-// import Single from "../singlePost/Single"
 
-<<<<<<< Updated upstream
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+ //Create function to fetch all items from database --- we will use useEffect hook 
 
-export default function Post() {
-  const { id } = useParams();
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    axios.get(`http://127.0.0.1:5001/item`).then((response) => {
-      setPost(response.data);
-    });
-  }, [id]);
-
-  // Check if post is null, if so, display loading message
-  if (post === null) {
-    return <div>Loading...</div>;
-  }
-
-=======
 export default function Post({img}) {
->>>>>>> Stashed changes
+  const [ListItems, setListItems] = useState([]);      
+
+  useEffect(()=>{
+    const getItemsList = async() =>{
+  
+      try{
+        const res = await axios.get('http://localhost:5001/item')
+        setListItems(res.data);
+        console.log('render')
+  
+      }catch(err){
+        console.log(err)
+      }
+    }
+    getItemsList();
+  }, []);
+
+
   return (
-    <div className="post w-96 mx-4 flex flex-col mt-8">
-      <Link to="/SinglePost-page">
-        <div className="flex flex-wrap justify-center p-6 rounded-xl shadow-lg hover:shadow-[#61AAC5] hover:bg-[#61AAC5] hover:text-white">
-          <img className="postImg w-96 h-72 object-cover"src={img} alt="" />
-          <div className="postTitle text-3xl font-semibold mt-4 cursor-pointer">
-            H-Awareness Article
-          </div>
-          <span className="postDate font-lora italic text-sm text-gray-600 mt-4">22nd August 2023 </span>
-          <p className="postDesc font-varela text-base leading-6 text-gray-700 mt-4 overflow-hidden overflow-ellipsis line-clamp-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-          </p>
+    <div className="flex flex-wrap justify-center mx-1 mb-10">
+      {ListItems.map((item) => (
+        <div key={item._id} className="post w-96 mx-4 flex flex-col mt-3">
+          <Link to={`/SinglePost-page/${item._id}`}>
+            <div className="flex flex-wrap justify-center p-6 rounded-xl shadow-lg hover:shadow-[#61AAC5] hover:bg-[#61AAC5] hover:text-white">
+              <img className="postImg w-96 h-72 object-cover" src={img} alt="" />
+              <div className="postTitle text-3xl font-semibold mt-4 cursor-pointer">
+                {item.title}
+              </div>
+              <span className="postDate font-lora italic text-sm text-gray-600 mt-4"> {new Date(item.date).toLocaleDateString()}</span>
+              <p className="postDesc font-varela text-base leading-6 text-gray-700 mt-4 overflow-hidden overflow-ellipsis line-clamp-4">
+                {item.content}
+              </p>
+            </div>
+          </Link>
         </div>
-      </Link>
+      ))}
     </div>
   );
 }
--- frontend/src/components/Single.jsx
+++ frontend/src/components/Single.jsx
@@ -1,14 +1,121 @@
-import { Link } from "react-router-dom";
-// import "./singlePost.css";
+// import { Link } from "react-router-dom";
+// // import "./singlePost.css";
 
+
+// export default function SinglePost() {
+//   return (
+//     <div className="singlePost bg-base-200 pb-10">
+//       <div className="navbar px-5"> 
+//         <button type="button" class="flex items-center justify-center w-1/3 sm:w-auto px-2 py-2 text-sm text-gray-700 transition-colors duration-200 bg-blue-300 border rounded-lg gap-x-2 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+//           <svg class="w-4 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+//             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+//           </svg>
+//           <span> 
+//             <Link to="/health-awareness" >
+//               Go back
+//             </Link>
+//           </span>
+//         </button>
+//       </div>
+     
+//       <div className="singlePostWrapper px-5">
+//         <div className="flex items-center justify-center">
+//           <img
+//             className="singlePostImg w-50 h-50 rounded-lg object-cover "
+//             src="https://cdn.pixabay.com/photo/2013/11/20/09/36/virus-213708_640.jpg"
+//             alt=""
+//           />
+//         </div>
+
+//         <h1 className="singlePostTitle text-center my-4 text-xl lg:text-2xl font-serif">
+//           Health Awareness Article
+//         </h1>
+
+//         <div className="singlePostInfo mb-5 px-5 md:px-10 flex justify-between italic text-sm md:text-md text-yellow-500 font-varela ">
+//           <span>
+//             Source:
+//             <b className="singlePostAuthor ml-1">
+//               <Link className="link" to="/posts?username=Safak">
+//                 BBS News
+//               </Link>
+//             </b>
+//           </span>
+//           <span>22nd August 2023</span>
+//         </div>
+        
+//         <p className="singlePostDesc text-gray-600 text-md px-5 md:px-10 text-justify lg:text-lg">
+//           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+//           quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+//           Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+//           eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+//           error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+//           impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+//           odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+//           elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+//           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+//           a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+//           elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+//           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+//           a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+//           elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+//           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+//           a odit modi eos!
+//           <br />
+//           <br />
+//           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+//           quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+//           Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+//           eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+//           error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+//           impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+//           odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+//           elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+//           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+//           a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+//         </p>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+
+import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function SinglePost() {
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const res = await axios.get(`http://localhost:5001/item/${id}`);
+        setItem(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchItem();
+  }, [id]); // Empty dependency array ensures the effect runs once after the initial render
+
+  if (item === null) {
+    return <div>Loading...</div>; // You can replace this with a loading spinner or any other loading indicator
+  }
+
+  const { title, content, date } = item;
+
   return (
     <div className="singlePost bg-base-200 pb-10">
-      <div className="navbar px-5"> 
-        <button type="button" class="flex items-center justify-center w-1/3 sm:w-auto px-2 py-2 text-sm text-gray-700 transition-colors duration-200 bg-blue-300 border rounded-lg gap-x-2 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
-          <svg class="w-4 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+      <h1 className="singlePostTitle text-center my-4 text-xl lg:text-2xl font-serif">
+        {title}
+      </h1>
+      <div className="navbar px-5">
+        <button type="button" className="flex items-center justify-center w-1/3 sm:w-auto px-2 py-2 text-sm text-gray-700 transition-colors duration-200 bg-blue-300 border rounded-lg gap-x-2 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+          <svg className="w-4 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
           </svg>
           <span> 
             <Link to="/health-awareness" >
@@ -17,64 +124,16 @@ export default function SinglePost() {
           </span>
         </button>
       </div>
-     
       <div className="singlePostWrapper px-5">
-        <div className="flex items-center justify-center">
-          <img
-            className="singlePostImg w-50 h-50 rounded-lg object-cover "
-            src="https://cdn.pixabay.com/photo/2013/11/20/09/36/virus-213708_640.jpg"
-            alt=""
-          />
-        </div>
-
-        <h1 className="singlePostTitle text-center my-4 text-xl lg:text-2xl font-serif">
-          Health Awareness Article
-        </h1>
-
-        <div className="singlePostInfo mb-5 px-5 md:px-10 flex justify-between italic text-sm md:text-md text-yellow-500 font-varela ">
-          <span>
-            Source:
-            <b className="singlePostAuthor ml-1">
-              <Link className="link" to="/posts?username=Safak">
-                BBS News
-              </Link>
-            </b>
-          </span>
-          <span>22nd August 2023</span>
-        </div>
-        
+        {/* ... */}
         <p className="singlePostDesc text-gray-600 text-md px-5 md:px-10 text-justify lg:text-lg">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos!
-          <br />
-          <br />
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+          {content}
+        </p>
+        {/* ... */}
+        <p className="singlePostDesc text-gray-600 text-md px-5 md:px-10 text-justify lg:text-lg">
+          {date}
         </p>
       </div>
-
     </div>
   );
 }
