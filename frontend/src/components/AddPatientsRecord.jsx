b935047c329edfc4ef9f687d4378edf41eb0ef3e
PemaLW <pemalw99@gmail.com>
2023-11-08 03:38:34 +0600

    update

--- frontend/src/components/AddPatientsRecord.jsx
+++ frontend/src/components/AddPatientsRecord.jsx
@@ -159,25 +159,24 @@ const AddPatientRecord=(props)=>{
     }
 
   return (
-
     <div className="flex flex-col justify-center space-y-8">
-        <h1 className="text-xl pl-10 pb-4 font-bold text-center">Add Patient Records</h1>
-            <form className="">
-                <div className="space-y-4">
-                    <div className="flex justify-center flex-col md:flex-row md:space-x-6">
-                        <input type="number"  className="input input-bordered w-1/4 max-w-xs" value={ (filteredList)?filteredList.colId:"000000"} />
-                        <input type="name"  className="input input-bordered w-1/4 max-w-xs" value={(filteredList)?filteredList.clientName:"xxxxxx"}/>
-                        <input type="text"  className="input input-bordered w-1/4 max-w-xs" value={(filteredList)?(filteredList.appointDate+filteredList.appointTime):"dd/mm/yyyy"}/>
-                    </div>
-                    <div className="flex flex-wrap justify-center space-y-4">
-                        <textarea className="textarea textarea-bordered h-40 w-4/5" placeholder="Diagnosis" onChange={(e) => setDiag(e.target.value)}></textarea>
-                        <input type="text" placeholder="Medicine Prescribed" className="input input-bordered h-20  w-4/5" onChange={(e) => setMedPrescribed(e.target.value)}/>
-                    </div>
-                    <div className="flex justify-center">
-                        <button className="btn bg-[#bcdbe6] hover:bg-gradient-to-r from-[#2f5d6e] to-[#5c8a9c] hover:text-white w-32 mt-8 mb-12" type="submit" onClick={add}>Generate Report</button>
-                    </div>
-                </div>
-            </form>
+      <h1 className="text-xl pl-10 pb-4 font-bold text-center">Add Patient Records</h1>
+      <form className="">
+        <div className="space-y-4">
+          <div className="flex justify-center flex-col md:flex-row md:space-x-6">
+              <input type="number"  className="input input-bordered w-1/4 max-w-xs" value={ (filteredList)?filteredList.colId:"000000"} />
+              <input type="name"  className="input input-bordered w-1/4 max-w-xs" value={(filteredList)?filteredList.clientName:"xxxxxx"}/>
+              <input type="text"  className="input input-bordered w-1/4 max-w-xs" value={(filteredList)?(filteredList.appointDate+filteredList.appointTime):"dd/mm/yyyy"}/>
+          </div>
+          <div className="flex flex-wrap justify-center space-y-4">
+              <textarea className="textarea textarea-bordered h-40 w-4/5" placeholder="Diagnosis" onChange={(e) => setDiag(e.target.value)}></textarea>
+              <input type="text" placeholder="Medicine Prescribed" className="input input-bordered h-20  w-4/5" onChange={(e) => setMedPrescribed(e.target.value)}/>
+          </div>
+          <div className="flex justify-center">
+              <button className="btn bg-[#bcdbe6] hover:bg-gradient-to-r from-[#2f5d6e] to-[#5c8a9c] hover:text-white w-32 mt-8 mb-12" type="submit" onClick={add}>Generate Report</button>
+          </div>
+        </div>
+      </form>
     </div>
     
   );
