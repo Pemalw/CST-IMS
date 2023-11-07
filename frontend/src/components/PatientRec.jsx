a10feb451883075e08e0f30fc70809537525962e
Taphu-github <99734455+Taphu-github@users.noreply.github.com>
2023-11-08 04:54:34 +0600

    added search bar
    This will help him retrieve the patient faster

--- frontend/src/components/PatientRec.jsx
+++ frontend/src/components/PatientRec.jsx
@@ -1,21 +1,16 @@
 import {React,useState, useEffect} from 'react';
-import AddPatientRecord from './AddPatientsRecord';
 import axios from 'axios';
-import PatientRec from './PatientRec';
 
-function Table() {
+function PatientRec() {
   const [appointments, setAppointments]=useState([]);
-  const [filteredList, setFilterList]=useState([]);
-  const [selectedId, setSelectedId] = useState();
+  
 
   useEffect( () => {
     async function fetchdata(){
         await axios.get('http://127.0.0.1:5001/appointment')
     .then((response) => {
         setAppointments(response.data);
-        const filterList = appointments.filter(appointment => appointment.state === 'yes');
-        filterList.reverse();
-        setFilterList(filterList);
+        
 
     })
     .catch((error) => {
@@ -23,7 +18,7 @@ function Table() {
     });
     }
     fetchdata();
-    }, [appointments]
+    }, []
 );
 function calculateAge(dateOfBirth) {
   const birthDate = new Date(dateOfBirth);
@@ -38,16 +33,31 @@ function calculateAge(dateOfBirth) {
 
   return age;
 }
-const openModal = (id) => {
-  const modal = document.getElementById('my_modal');
-  modal.showModal();
-};
+
+const handleSearch = () => {
+    const searchTerm = document.getElementById('search').value.toLowerCase();
+  
+    const filteredAppointments = appointments.filter(app => {
+      return (
+        app.clientName.toLowerCase().includes(searchTerm) ||
+        app.email.toLowerCase().includes(searchTerm) ||
+        String(app.colId).toLowerCase().includes(searchTerm)
+      );
+    });
+  
+    // Update the appointments list with the filtered results
+    setAppointments(filteredAppointments);
+  };
 
   return (
     <div className="flex flex-col justify-center my-16">
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-20">
+        <div className="felx search-bar justify-center">
+            <input type="text" placeholder="Search by name, email, or contact number" id="search" />
+            <button onClick={() => handleSearch()}>Search</button>
+        </div>
         <table className="table-auto w-5/6">
-            <caption className='text-left md:text-2xl text-xl indent-4 font-bold drop-shadow-4 mb-10'>Patient List</caption>
+            <caption className='text-left text-xl md:text-2xl indent-4 font-bold drop-shadow-4 mb-10'>Patient Records</caption>
             <thead className='thead-light border-b-2'>
                 <tr>
                   <th>StdID</th>
@@ -57,14 +67,11 @@ const openModal = (id) => {
                   <th>ContactNo</th>
                   <th>Email</th>
                   <th>Time</th>
-                <th>
-                  
-                </th>
                 </tr>
             </thead>
             <tbody className='tbody-light'>
             
-                {filteredList.map(app => (
+                {appointments.map(app => (
                 <tr key={app._id}>
                     <td className="font-light text-left border-b p-4 " >{app.colId}</td>
                     <td className="font-light text-left border-b p-4 " >{app.clientName}</td>
@@ -73,33 +80,13 @@ const openModal = (id) => {
                     <td className="font-light text-left border-b p-4 " >{app.contactNo}</td>
                     <td className="font-light text-left border-b p-4 " >{app.email}</td>
                     <td className="font-light text-left border-b p-4 " >{app.appointTime}</td>
-                    
-                    <td>
-                        <button className="btn btn-sm normal-case bg-[#003046] text-base-100 btn-accent border-0" onClick={()=>{openModal(app._id)
-                        setSelectedId(app._id)}}>
-                            Generate Report
-                        </button>
-                        <dialog id="my_modal" className="modal">
-                          <div className="modal-box w-11/12 max-w-5xl bg-slate-50">
-                            <form method="dialog">
-                              {/* if there is a button in form, it will close the modal */}
-                              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                            </form>
-                            <AddPatientRecord data={selectedId}/>
-                            <div className="modal-action">
-                            </div>
-                          </div>
-                        </dialog>
-                    </td>
                 </tr>
                 ))}
             </tbody>
         </table>
       </div>
-      
-      <PatientRec/>
     </div>
   );
 }
 
-export default Table;
+export default PatientRec;
