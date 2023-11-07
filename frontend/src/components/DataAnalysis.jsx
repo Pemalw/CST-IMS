3603fb86e9100bd05caf9fa312a36d98e7dba313
Taphu-github <99734455+Taphu-github@users.noreply.github.com>
2023-11-08 00:27:01 +0600

    working la
    Data analysis is disturbing

--- frontend/src/components/DataAnalysis.jsx
+++ frontend/src/components/DataAnalysis.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-import GradientLineGraph from './GradientLineGraph'
+import React from 'react';
+import GradientLineGraph from './GradientLineGraph';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
 
 const DataAnalysis = () => {
+  const [report, setReport]=useState([]);
+  const [studentCount, setStudentCounts]=useState(0);
+  const [inventoryItems, setinventoryItems] = useState([]);  
+
+
+  useEffect( () => {
+    async function fetchdata(){
+        await axios.get('http://127.0.0.1:5001/report')
+    .then((response) => {
+        setReport(response.data);
+        setStudentCounts(report.length);
+        
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+    }
+    fetchdata();
+    }, [report]
+  );
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-col">
@@ -16,7 +39,7 @@ const DataAnalysis = () => {
           <div className="stats shadow stats-vertical lg:stats-horizontal w-64 h-40">
             <div className="stat place-items-center">
               <div className="stat-title">Total Consultations</div>
-              <div className="stat-value text-[#61AAC5]">56</div>
+              <div className="stat-value text-[#61AAC5]">{studentCount}</div>
               <div className="stat-desc">Total number of students visted in a month</div>
             </div>
           </div>
